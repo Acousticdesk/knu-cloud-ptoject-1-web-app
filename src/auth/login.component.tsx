8a104741f611654ca6d051e22f308ff119aeb6b1
Andrii Kicha <akicha@star.global>
2023-02-12 12:19:16 +0200

    update user id context

--- src/auth/login.component.tsx
+++ src/auth/login.component.tsx
@@ -1,9 +1,23 @@
 import { Box, Input } from "@chakra-ui/react";
+import { authContext } from "./auth.context";
+import { ChangeEvent } from "react";
+
+const { Consumer: AuthContextConsumer } = authContext;
 
 export function Login({}) {
   return (
-    <Box>
-      <Input placeholder="User ID" />
-    </Box>
+    <AuthContextConsumer>
+      {({ userId, setUserId }) => (
+        <Box>
+          <Input
+            placeholder="User ID"
+            value={!userId ? undefined : userId}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setUserId(e.target.value)
+            }
+          />
+        </Box>
+      )}
+    </AuthContextConsumer>
   );
 }
